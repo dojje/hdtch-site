252e1cd4de35d67ac866c77634628b5f1ff48593
Daniel Hidefjäll <50925050+dojje@users.noreply.github.com>
2026-08-27 20:58:56 +0200

    deploy 2026-08-27

---
+++ _astro/BaseLayout.astro_astro_type_script_index_0_lang.DO70xnfg.js
@@ -0,0 +1,1 @@
+import{i as t,t as e}from"./i18n.BZRnn6uj.js";t();document.getElementById("lang-toggle")?.addEventListener("click",e);
--- _astro/BaseLayout.astro_astro_type_script_index_0_lang.Tcjmxq7A.js
+++
@@ -1,1 +0,0 @@
-import{i as t,t as e}from"./i18n.C9QXCil8.js";t();document.getElementById("lang-toggle")?.addEventListener("click",e);
